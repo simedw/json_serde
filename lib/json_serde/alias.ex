bd36a961226a72cf3744e1bec3adf2d1cc00a9c0
Simon Edwardsson <simon.edwardsson@gmail.com>
2024-05-28 20:32:47 +0100

    no Code.ensure_loaded? for performance reasons

--- lib/json_serde/alias.ex
+++ lib/json_serde/alias.ex
@@ -37,25 +37,11 @@ defmodule JsonSerde.Alias do
 
   @spec to_alias(module) :: String.t()
   def to_alias(module) do
-    Map.get_lazy(@to_aliases, module, fn ->
-      custom_module = Module.concat(JsonSerde.Custom.Modules, module)
-
-      case Code.ensure_loaded?(custom_module) do
-        true -> apply(custom_module, :alias, [])
-        false -> to_string(module)
-      end
-    end)
+    Map.get_lazy(@to_aliases, module, fn -> to_string(module) end)
   end
 
   @spec from_alias(String.t()) :: module
   def from_alias(alias) do
-    Map.get_lazy(@from_aliases, alias, fn ->
-      custom_module = Module.concat(JsonSerde.Custom.Aliases, alias)
-
-      case Code.ensure_loaded?(custom_module) do
-        true -> apply(custom_module, :module, [])
-        false -> String.to_atom(alias)
-      end
-    end)
+    Map.get_lazy(@from_aliases, alias, fn -> String.to_atom(alias) end)
   end
 end
